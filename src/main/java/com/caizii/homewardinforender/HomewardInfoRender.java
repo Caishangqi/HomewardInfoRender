f74aae605e5cfc295c892b575e759798e9ddc675
Caishangqi <Caiziii@126.com>
2022-10-30 07:49:00 +0800

    Initial commit

---
+++ src/main/java/com/caizii/homewardinforender/HomewardInfoRender.java
@@ -0,0 +1,17 @@
+package com.caizii.homewardinforender;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomewardInfoRender extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
