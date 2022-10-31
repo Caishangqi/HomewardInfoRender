8793f1dd5eb54020d31a81a01ee21b3a352cabde
Caishangqi <Caiziii@126.com>
2022-11-01 03:29:51 +0800

    demo ProtocolLib 1

--- src/main/java/com/caizii/HomewardInfoRender.java
+++ src/main/java/com/caizii/HomewardInfoRender.java
@@ -5,9 +5,15 @@ import com.caizi.utils.logs.ConsoleLogger;
 import com.caizi.utils.logs.CustomLogger;
 import com.caizi.utils.logs.LoggerManipulationType;
 import com.caizii.command.MainCommand;
+import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HomewardInfoRender extends JavaPlugin {
@@ -30,7 +36,34 @@ public final class HomewardInfoRender extends JavaPlugin {
         // Plugin startup logic
         initializedComponents();
         registerCommands();
+        //registerPackageListener();
         loadConfigurations();
+
+    }
+
+    private void registerPackageListener() {
+
+
+        //Listening to an Incoming packet - from the CLIENT to SERVER
+        protocolManager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.POSITION) {
+
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player player = event.getPlayer();
+
+                //Extracting information from the packet
+
+                double x = packet.getDoubles().read(0);
+                double y = packet.getDoubles().read(1);
+                double z = packet.getDoubles().read(2);
+
+                boolean isOnGround = packet.getBooleans().read(0);
+                customLogger.send("INBOUND PACKET: x = " + x + "y = " + y + " z = " + z + "isOnGround() = " + isOnGround, player);
+
+            }
+        });
+
 
     }
 
@@ -48,7 +81,7 @@ public final class HomewardInfoRender extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        consoleLogger.send(LoggerManipulationType.UNLOAD,"关闭 "+this.getName()+" 中...");
+        consoleLogger.send(LoggerManipulationType.UNLOAD, "关闭 " + this.getName() + " 中...");
     }
 
     /**
@@ -58,13 +91,14 @@ public final class HomewardInfoRender extends JavaPlugin {
         plugin = this;
         consoleLogger = new ConsoleLogger(pluginPrefix);
         customLogger = new CustomLogger(pluginPrefix);
-        consoleLogger.send(LoggerManipulationType.LOAD,"正在初始化 " +this.getName()+ " 的必要组件");
+        consoleLogger.send(LoggerManipulationType.LOAD, "正在初始化 " + this.getName() + " 的必要组件");
         //初始化ProtocolLib
         protocolManager = ProtocolLibrary.getProtocolManager();
     }
 
     public static HomewardInfoRender getInstance() {
-        if (plugin != null) return plugin;else throw new RuntimeException("plugin has not been initialized!");
+        if (plugin != null) return plugin;
+        else throw new RuntimeException("plugin has not been initialized!");
     }
 
 }
