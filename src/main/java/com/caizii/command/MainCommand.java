2e56381fb937bbb29e79be0bd4741673cb6bcb16
Caishangqi <Caiziii@126.com>
2022-11-14 07:02:12 +0800

    测试bossbar通过包新式发送

--- src/main/java/com/caizii/command/MainCommand.java
+++ src/main/java/com/caizii/command/MainCommand.java
@@ -6,13 +6,20 @@ import com.caizi.mf.annotations.SubCommand;
 import com.caizi.mf.base.CommandBase;
 import com.caizii.HomewardInfoRender;
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.InternalStructure;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import me.clip.placeholderapi.libs.kyori.adventure.bossbar.BossBar;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
 
 import static com.caizii.HomewardInfoRender.protocolManager;
 
@@ -60,6 +67,30 @@ public class MainCommand extends CommandBase {
             }
 
         });
+
+    }
+
+    @SubCommand("showBossBar")
+    public void demoBossBarShow(final CommandSender commandSender) {
+        Player player = (Player) commandSender;
+        HomewardInfoRender.customLogger.send("show the demo boss bar to player", player);
+
+        PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.BOSS);
+        packet.getModifier().write(0, UUID.randomUUID());
+        InternalStructure internalStructure = packet.getStructures().read(1);
+        internalStructure.getChatComponents().write(0,WrappedChatComponent.fromText("Demo Test BossBar"));
+        internalStructure.getFloat().write(0,1F);
+        internalStructure.getEnumModifier(BarColor.class, 2).write(0, BarColor.YELLOW);
+        //internalStructure.getEnumModifier(BarStyle.class, 3).write(0, BarStyle.SOLID);
+        internalStructure.getModifier().write(4, false);
+        internalStructure.getModifier().write(5, false);
+        internalStructure.getModifier().write(6, false);
+
+        try {
+            protocolManager.sendServerPacket(player, packet);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
